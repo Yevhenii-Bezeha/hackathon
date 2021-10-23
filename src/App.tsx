b660f9a66e059f9d93ab0f8b852ddb6342547d4c
Vlad Zusko <vladzvu11@gmail.com>
2021-10-23 19:39:02 +0300

    Fixed app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { Router } from 'containers';
 import 'styles/index.scss';
-import PokemonList from 'components/pages/pokemonList';
-import Modal from './components/common/modal';
-function App(): JSX.Element {
+
+const App = (): JSX.Element => {
   return (
     <Provider store={store}>
       <Router />
-      <Modal>
-        <PokemonList />
-      </Modal>
     </Provider>
   );
-}
+};
 
 export default App;
