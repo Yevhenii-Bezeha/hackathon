5638f26f62c43bf4832ec475ccca658690aef05d
Vlad Zusko <vladzvu11@gmail.com>
2021-10-23 14:53:57 +0300

    Added navigation

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,1 @@
-export {};
+export { default as Navigation } from './navigation';
---
+++ src/components/common/navigation/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import clsx from 'clsx';
+import { NavLink } from 'react-router-dom';
+import { Routes } from 'common';
+import { Logo } from 'components';
+import styles from './navigation.module.scss';
+
+const Navigation = (): JSX.Element => {
+  return (
+    <nav className={styles.navigation}>
+      <div className={clsx('container', styles.navigation__container)}>
+        <div className={styles.navigation__part}>
+          <Logo />
+          <NavLink
+            to={Routes.ROOT}
+            className={styles.navigation__link}
+            activeClassName={styles.navigation__link_active}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={Routes.ALL}
+            className={styles.navigation__link}
+            activeClassName={styles.navigation__link_active}
+          >
+            Pokemons
+          </NavLink>
+          <NavLink
+            to={Routes.ABOUT}
+            className={styles.navigation__link}
+            activeClassName={styles.navigation__link_active}
+          >
+            About
+          </NavLink>
+        </div>
+
+        <div className={styles.navigation__part}>
+          <NavLink
+            to={Routes.LOGIN}
+            className={styles.navigation__link}
+            activeClassName={styles.navigation__link_active}
+          >
+            Login
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
