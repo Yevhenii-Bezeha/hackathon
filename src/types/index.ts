2f05e00c5209d6fbfbdf02abfee1df9c96a149b0
Vlad Zusko <vladzvu11@gmail.com>
2021-10-24 00:20:24 +0300

    Add test user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export type { default as IPokemon } from './pokemon';
 export type { default as IError } from './error';
 export type { default as ICredentials } from './credentials';
+export type { default as IUser } from './user';
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+interface IUser {
+  id: string;
+  login: string;
+  token: string;
+  name: string;
+}
+
+export default IUser;
