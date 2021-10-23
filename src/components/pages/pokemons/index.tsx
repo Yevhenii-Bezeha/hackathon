04ab0bff17cd78cf019d425a0e681f1674c8c8bd
Vlad Zusko <vladzvu11@gmail.com>
2021-10-24 01:30:56 +0300

    Added basic scroll

--- src/components/pages/pokemons/index.tsx
+++ src/components/pages/pokemons/index.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Footer, Navigation, Pokemon } from 'components';
 import { IPokemon } from 'types';
 import styles from './pokemons.module.scss';
 import { Routes } from 'common';
 
-type TPokemons = {
+type TPokemonsProps = {
   pokemons: IPokemon[];
+  pokemonsNumber: number;
+  onLoadMore: () => void;
 };
 
-const PokemonsPage = ({ pokemons }: TPokemons): JSX.Element => {
+const PokemonsPage = ({ pokemons, pokemonsNumber, onLoadMore }: TPokemonsProps): JSX.Element => {
+  const onLoadMoreHandler = () => {
+    if (onLoadMore) {
+      onLoadMore();
+    }
+  };
+
   return (
     <div>
       <Navigation />
       <main className={clsx('main', styles.main)}>
-        <div className={clsx('container', styles.main__container)}>
+        <InfiniteScroll
+          loader={<h1 className={styles.main__title}>Loading...</h1>}
+          dataLength={pokemons.length}
+          hasMore={pokemons.length < pokemonsNumber}
+          next={onLoadMoreHandler}
+          className={clsx('container', styles.main__container)}
+        >
           {pokemons.map((pokemon) => (
             <Pokemon pokemon={pokemon} link={`${Routes.ALL}/${pokemon.id}`} key={pokemon.id} />
           ))}
-        </div>
+        </InfiniteScroll>
       </main>
       <Footer />
     </div>
