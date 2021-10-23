795e94364c5576e03a42c2022774bcf894a5b32f
Vlad Zusko <vladzvu11@gmail.com>
2021-10-23 14:53:32 +0300

    Added some routes

--- src/common/enums/routes.ts
+++ src/common/enums/routes.ts
@@ -2,6 +2,10 @@ const Routes = {
   ROOT: '/',
   ALL: '/all',
   ONE: '/all/:id',
+  ABOUT: '/about',
+  LOGIN: '/login',
+  LOGOUT: '/logout',
+  PROFILE: '/profile',
 };
 
 export default Routes;
