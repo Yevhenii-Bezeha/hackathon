227a9c1bf88e6129e8c746936cf65910737f662b
Tofik Hasanov <annexcies@gmail.com>
2021-10-24 13:35:05 +0300

    delete dublicate in home page

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -13,7 +13,7 @@ const index = () => {
           <div className={styles.main__description}>
             <h1 className={styles.main__title}>Welcome, travelers! </h1>
             <div className={styles.main__text}>
-              <p>You have found what you have been looking for for a long time ;)</p>
+              <p>You have found what you have been looking for a long time ;)</p>
               <p>
                 Create your own Pokemon, leave reviews, share your findings with your friends.
                 Pokemon are back!
