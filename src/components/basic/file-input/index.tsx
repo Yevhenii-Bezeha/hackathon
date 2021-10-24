5beb261b0f2752ceaa674619827e135ad70a2275
Vlad Zusko <vladzvu11@gmail.com>
2021-10-24 14:29:02 +0300

    Fixed file input

--- src/components/basic/file-input/index.tsx
+++ src/components/basic/file-input/index.tsx
@@ -2,34 +2,23 @@ import React, { ChangeEvent, useState } from 'react';
 import { Button } from 'components';
 import styles from './file-input.module.scss';
 import clsx from 'clsx';
-import imageToBase64 from 'image-to-base64';
 
 type TSignInFormProps = {
   className?: string;
-  addFilePath?: (event: any) => void;
 };
 
-const FileInput = ({ className, addFilePath }: TSignInFormProps): JSX.Element => {
+const FileInput = ({ className }: TSignInFormProps): JSX.Element => {
   const [value, setValue] = useState<FileList | null>(null);
-  const [path, setPath] = useState('');
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.currentTarget.files);
-    console.log(event.target.value);
-    // imageToBase64(event.target.value)
-    //   .then((response) => {
-    //     setPath(response);
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
   };
+
   return (
     <div>
       <Button className={clsx(styles.button, className)}>
         <label className={styles.label} htmlFor="file-input">
-          {value ? value[0].name : 'Select a picture'}
+          {value ? value[0].name : 'Select a file'}
         </label>
         <input
           className={styles.input}
