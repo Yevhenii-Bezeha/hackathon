7707ac6a7a9205d19dec17f800ad52ce5205902d
Vlad Zusko <vladzvu11@gmail.com>
2021-10-24 09:55:25 +0300

    Fixed file input

--- src/components/basic/file-input/index.tsx
+++ src/components/basic/file-input/index.tsx
@@ -1,23 +1,27 @@
+import React, { ChangeEvent, useState } from 'react';
 import { Button } from 'components';
-import React, { ChangeEvent, useEffect, useState } from 'react';
 import styles from './file-input.module.scss';
 
 const FileInput = () => {
   const [value, setValue] = useState<FileList | null>(null);
 
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.files);
+  };
+
   return (
     <div>
       <Button className={styles.button}>
-        <label className={styles.label} htmlFor="avatar">
+        <label className={styles.label} htmlFor="file-input">
           {value ? value[0].name : 'Select a picture'}
         </label>
         <input
           className={styles.input}
           type="file"
-          id="avatar"
+          id="file-input"
           name="avatar"
           accept="image/png, image/jpeg"
-          onChange={(event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.files)}
+          onChange={onChangeHandler}
         />
       </Button>
     </div>
